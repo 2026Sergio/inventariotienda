aca5d82c794013b53d2c6afd3737c95a3449cbcc
Sergio Ajú <mirandasegio550@gmail.com>
2026-03-08 16:04:44 -0600

    feat: :sparkles: Add menu feature
    Add menu feature with conditional logic

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 def menu_principal():
-    pass
+    menu = """
+BIENVENIDO 
+1. AGREGAR PRODUCTO
+2. LISTA PRODUCTOS
+3. ACTUALIZAR CANTIDAD
+4. ELIMINAR PRODUCTO
+5. CALCULAR VALOR TOTAL DEL INVENTARIO
+6. SALIR
+"""
+    print(menu)
+
+
+while True:
+    menu_principal()
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        print("Agregar producto")
+
+    elif opcion == "2":
+        print("Lista de productos")
+
+    elif opcion == "3":
+        print("Actualizar cantidad")
+
+    elif opcion == "4":
+        print("Eliminar producto")
+
+    elif opcion == "5":
+        print("Calcular valor total")
+
+    elif opcion == "6":
+        print("Saliendo del programa...")
+        break
+
+    else:
+        print("Opción no válida")
 
 def lista_producto():
-    pass
+    print("*"*15)
 
 def Actualizar_producto():
     pass
@@ -14,4 +50,5 @@ def Calcular_valor_inventario():
     pass
 
 def salir():
-    pass
+    print("Saliendo del programa...")
+    exit()
