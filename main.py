78062b86e1fdf78c0ef0d7428365b07c61b954ba
Sergio Ajú <mirandasegio550@gmail.com>
2026-03-08 15:19:32 -0600

    feat: :sparkles: Funtions develop
    Add funtions structure in main file

---
+++ main.py
@@ -0,0 +1,17 @@
+def menu_principal():
+    pass
+
+def lista_producto():
+    pass
+
+def Actualizar_producto():
+    pass
+
+def Eliminar_producto():
+    pass
+
+def Calcular_valor_inventario():
+    pass
+
+def salir():
+    pass
